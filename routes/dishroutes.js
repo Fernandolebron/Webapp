8a88824d4b763aa007d2fa93e13c36ca0150db41
isisgomez28 <isisgomez.28@hotmail.es>
2015-10-13 14:35:39 +0000

    Habilitando middleware del dishRoute

--- routes/dishroutes.js
+++ routes/dishroutes.js
@@ -8,34 +8,31 @@ var jwt = require('json-web-token');
 // route middleware to verify a token
 router.use(function(req, res, next) {
 	console.log('using dish controller');
-  // check header or url parameters or post parameters for token
-  //var token = req.body.token || req.query.token || req.headers['x-access-token'];
-
-  //// decode token
-  //if (token) {
-
-  //  // verifies secret and checks exp
-  //  jwt.verify(token, 'SecretKey', function(err, decoded) {      
-  //    if (err) {
-  //      return res.json({ success: false, message: 'Failed to authenticate token.' });    
-  //    } else {
-  //      // if everything is good, save to request for use in other routes
-  //      req.decoded = decoded;    
-  //      next();
-  //    }
-  //  });
-
-  //} else {
-
-  //  // if there is no token
-  //  // return an error
-  //  return res.status(403).send({ 
-  //      success: false, 
-  //      message: 'No token provided.' 
-  //  });
-    
-  //}
-  next();
+	// check header or url parameters or post parameters for token
+	var token = req.body.token || req.query.token || req.headers['x-access-token'];
+	
+	// decode token
+	if (token) {
+	// verifies secret and checks exp
+	jwt.verify(token, 'SecretKey', function(err, decoded) {      
+	  if (err) {
+	    return res.json({ success: false, message: 'Failed to authenticate token.' });    
+	  } else {
+	    // if everything is good, save to request for use in other routes
+	    req.decoded = decoded;    
+	    next();
+	  }
+	});
+	
+	} else {
+		// if there is no token
+		// return an error
+		return res.status(403).send({ 
+		    success: false, 
+		    message: 'No token provided.' 
+		});
+	}
+	next();
 });
 
 //	Retornar un plato en específico
