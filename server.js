62b58f7ba9db96a3b75e7062161f8a449dd51cb4
isisgomez28 <igomezp.28@gmail.com>
2015-11-04 10:49:52 -0400

    cambio de puerto en Digital Ocean

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Asigna puerto
-var port = 8080;
+var port = 8082;
 
 // REGISTER OUR ROUTES -------------------------------
 app.use('/user', userRoutes);
